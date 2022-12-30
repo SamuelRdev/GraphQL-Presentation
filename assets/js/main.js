6989c685b57673420f89d1a353112cb14ca2385c
Samuel R <the.sam.r@hotmail.fr>
2022-12-30 23:16:58 +0100

    responsive fait (xd bonne guebla)

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,4 +7,32 @@ $(document).ready(function(){
             $('#navigation').removeClass('sticky-style');
         }
     })
+
+    // if($(window).innerWidth)
+    // if($(window).innerWidth() < 992){
+    //     $('.cards').slick({
+    //         arrows: false,
+    //         autoplay: false,
+    //         autoplaySpeed: 3000,
+    //         dots: false,
+    //         draggable: true,
+    //         fade: true,
+    //         infinite: true,
+    //         slidesToShow: 2
+            // responsive: [
+            // {
+            //   breakpoint: 992,
+            //   settings: {
+            //       arrows: true
+            //   }
+            // },
+            // {
+            //   breakpoint: 345,
+            //   settings: {
+            //       arrows: true
+            //   }
+            // }
+            // ]
+        // });
+    // }
 })
